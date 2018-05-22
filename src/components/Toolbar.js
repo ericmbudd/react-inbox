5085f3e37e6e347b066d3253b5f0cd8964552093
Eric Budd <ericbudd@gmail.com>
2018-05-22 11:52:50 -0600

    seed file into json

--- src/components/Toolbar.js
+++ src/components/Toolbar.js
@@ -2,7 +2,7 @@ import React from 'react'
 
 const Toolbar = ( { copyright } ) => {
   // console.log(copyright);
-  return ( <footer className="page-footer">
+  return ( <footer className="toolbar">
     <div className="container">
       <div className="row">
         <div className="col l6 s12">
