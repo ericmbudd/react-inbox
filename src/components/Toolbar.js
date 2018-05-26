3ff365fefb4879e0d22f690ccf9bc62568e2a393
Eric Budd <ericbudd@gmail.com>
2018-05-26 11:59:36 -0600

    checkbox status working

--- src/components/Toolbar.js
+++ src/components/Toolbar.js
@@ -2,18 +2,33 @@ import React from 'react'
 
 const Toolbar = ( { messages, selectAll } ) => {
   console.log( "in toolbar, messages", messages );
-  let messageCount = messages.filter( each => each.read === false ).length
-  console.log( "messageCount", messageCount )
+  let unreadCount = messages.filter( each => each.read === false ).length
+  let selectedCount = messages.filter( each => each.selected === true ).length
+
+  // console.log( "selectedCount", selectedCount );
+  // console.log( "messages.length", messages.length );
+
+  let selectedStatus = "fa fa-square-o"
+
+  selectedCount > 0
+    ? selectedStatus = "fa fa-minus-square-o"
+    : selectedStatus
+
+  selectedCount === messages.length
+    ? selectedStatus = "fa fa-check-square-o"
+    : selectedStatus
+
+  //console.log( "unreadCount", unreadCount )
 
   return ( <div className="row toolbar">
     <div className="col-md-12">
       <p className="pull-right">
-        <span className="badge badge">{messageCount}</span>
+        <span className="badge badge">{unreadCount}</span>
         unread messages
       </p>
 
       <button className="btn btn-default">
-        <i className="fa fa-check-square-o"></i>
+        <i className={selectedStatus}></i>
       </button>
 
       <button className="btn btn-default">
